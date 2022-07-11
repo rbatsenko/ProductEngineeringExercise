bf43887fd2676269c748c2dc621298a4bfab6241
Roman Batsenko <rbatsenko@gmail.com>
2022-07-11 17:17:07 +0200

    Upgrade, add deps, setup code analysis

--- TODO.md
+++ TODO.md
@@ -1,14 +1,16 @@
 # List of tasks
 
 ### Structure, deps, environment
- - [ ] Upgrade dependencies, set `save-exact` to `true`
- - [ ] Add `TypeScript`
- - [ ] Add `ESLint`, `Prettier`
- - [ ] Configure pre-commit checks
- - [ ] Styling solution (`CSS Modules` / `Styled Components` / something else?)
- - [ ] Add CI/CD pipelines (`Vercel`?)
- 
+
+- [x] Upgrade dependencies, set `save-exact` to `true`
+- [x] Add `TypeScript`
+- [x] Add `ESLint`, `Prettier`
+- [x] Configure pre-commit checks
+- [x] Styling solution (Use `CSS Modules` - Ant Design provides basic styles already,`CSS Modules` will be a customization tool, supported out-of-the-box in `Next.js`)
+- [ ] Add CI/CD pipelines (`Vercel`?)
+
 ### UI
+
 - [ ] Sketch app layout and component tree (I would do it normally, but not in a 3,5h time frame 🙂)
 - [ ] Create app layout (use `Tabs` for switching between Boards?)
 - [ ] Decide on component state locations (https://reactjs.org/docs/lifting-state-up.html)
@@ -16,5 +18,6 @@
 - [ ] Mobile, tablet versions, responsiveness
 
 ### Testing
+
 - [ ] Unit, Integration tests with `Jest`, `React Testing Library`
 - [ ] E2E tests with `Cypress`
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,23 @@
-import axios from 'axios'
-import { Card, Col, Row } from 'antd'
-import 'antd/dist/antd.css'
+import axios from "axios";
+import { Card, Col, Row } from "antd";
+import "antd/dist/antd.css";
 
-const API_URL = "https://v1.nocodeapi.com/headsup/google_sheets/AgICuFGNAdLNjVaY"
+const API_URL =
+  "https://v1.nocodeapi.com/headsup/google_sheets/AgICuFGNAdLNjVaY";
 
-export default function KanbanPage({ personData, companyData }) {
+const KanbanPage = ({ personData, companyData }) => {
+  console.log(personData);
+  console.log(companyData);
+
   return (
-    <div style={{ margin: '100px' }}>
+    <div style={{ margin: "100px" }}>
       <h1>Kanban Board</h1>
       <div className="site-card-wrapper">
         <Row gutter={16}>
           <Col span={8}>
             <h2>Signed Up</h2>
             {companyData.map((company) => (
-              <div style={{ marginBottom: '16px' }}>
+              <div key={company.id} style={{ marginBottom: "16px" }}>
                 <Card title={company.name}>
                   <strong>Signed Up:</strong> {company.signed_up}
                 </Card>
@@ -23,7 +27,7 @@ export default function KanbanPage({ personData, companyData }) {
           <Col span={8}>
             <h2>At Least 2 Users</h2>
             {companyData.map((company) => (
-              <div style={{ marginBottom: '16px' }}>
+              <div key={company.id} style={{ marginBottom: "16px" }}>
                 <Card title={company.name}>
                   <strong>Signed Up:</strong> {company.signed_up}
                 </Card>
@@ -33,7 +37,7 @@ export default function KanbanPage({ personData, companyData }) {
           <Col span={8}>
             <h2>At Least 3 Comments</h2>
             {companyData.map((company) => (
-              <div style={{ marginBottom: '16px' }}>
+              <div key={company.id} style={{ marginBottom: "16px" }}>
                 <Card title={company.name}>
                   <strong>Signed Up:</strong> {company.signed_up}
                 </Card>
@@ -44,26 +48,31 @@ export default function KanbanPage({ personData, companyData }) {
       </div>
     </div>
   );
-}
+};
 
-export async function getStaticProps(context) {
-
-  const { data: { data: personData } } = await axios.get(API_URL, {
+export const getStaticProps = async (_context) => {
+  const {
+    data: { data: personData },
+  } = await axios.get(API_URL, {
     params: {
-      tabId: 'person'
-    }
-  })
+      tabId: "person",
+    },
+  });
 
-  const { data: { data: companyData } } = await axios.get(API_URL, {
+  const {
+    data: { data: companyData },
+  } = await axios.get(API_URL, {
     params: {
-      tabId: 'company'
-    }
-  })
+      tabId: "company",
+    },
+  });
 
   return {
     props: {
       personData,
-      companyData
-    }
-  }
-}
+      companyData,
+    },
+  };
+};
+
+export default KanbanPage;
